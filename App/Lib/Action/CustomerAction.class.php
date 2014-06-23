7f1537188d864917399edb7ecae11098f2ac537a
smeoa <smeoa@qq.com>
2014-06-23 12:00:01 +0800

    客户修复

--- App/Lib/Action/CustomerAction.class.php
+++ App/Lib/Action/CustomerAction.class.php
@@ -213,7 +213,7 @@ class CustomerAction extends CommonAction {
 		if (false === $model -> create()) {
 			$this -> error($model -> getError());
 		}
-		$model ->letter=get_letter($model ->'name');
+		$model ->letter=get_letter($model ->name);
 		//保存当前数据对象
 		$list = $model -> add();
 		if ($list !== false) {//保存成功
@@ -231,7 +231,7 @@ class CustomerAction extends CommonAction {
 		if (false === $model -> create()) {
 			$this -> error($model -> getError());
 		}
-		$model ->letter=get_letter($model ->'name');
+		$model ->letter=get_letter($model ->name);
 
 		// 更新数据
 		$list = $model -> save();
