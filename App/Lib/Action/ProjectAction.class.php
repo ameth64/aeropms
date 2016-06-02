bba8d20646228ce11db4f687962629c2d4829f3f
MobiuS <ameth@qq.com>
2016-06-02 14:46:38 +0800

    Add system table sql and aeropms_common.js for project specific operations

--- App/Lib/Action/ProjectAction.class.php
+++ App/Lib/Action/ProjectAction.class.php
@@ -4,7 +4,12 @@ class ProjectAction extends CommonAction {
 
 	public function select()
 	{
+        $model = M("ProjectList");
+        $proj_list = $model->select();
+        $this->assign("proj_list", $proj_list);
+        $this->assign("title", get_system_config("SYSTEM_NAME")." - 项目列表");
 		$this->display();
 	}
 }
+
 ?>
